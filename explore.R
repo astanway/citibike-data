cd8b6e63deed60513076eee34b31235bf57efd50
Abe Stanway <abestanway@gmail.com>
2014-03-12 19:43:25 -0400

    Analysis complete\!

--- explore.R
+++ explore.R
@@ -28,16 +28,36 @@ com = data.frame(arrivals, one)
 com = xts(com[,-1], order.by=com[,1])
 arrivals = period.apply(com, endpoints(com, "hours"), sum)
 
-one = rep(c(1), length(departures))
-com = data.frame(departures, one)
+# Get the trips
+json_file <- "~/code/citibike/departures.json"
+json_data <- fromJSON(paste(readLines(json_file), collapse=""))
+date = names(unlist(json_data))
+date = as.POSIXlt(as.numeric(date), origin="1970-01-01")
+values = unname(unlist(json_data))
+com = data.frame(date, values)
 com = xts(com[,-1], order.by=com[,1])
-departures = period.apply(com, endpoints(com, "hours"), sum)
+departures = period.apply(com, endpoints(com, "days"), sum)
+plot(departures)
 
-one = rep(c(1), length(combined))
-com = data.frame(combined, one)
+# Plot the temps
+par(new = TRUE)
+temps = read.csv("~/code/citibike/temperatures/temps.csv", header=TRUE)
+temps$datetime = as.POSIXct(temps$datetime)
+com = data.frame(temps$datetime, temps$temp)
 com = xts(com[,-1], order.by=com[,1])
-both = period.apply(com, endpoints(com, "hours"), sum)
+both = period.apply(com, endpoints(com, "days"), mean)
+plot(both, type='l', col="blue", lwd=2, ylim =c(-0, 100),xaxt="n",yaxt="n",xlab="",ylab="")
+axis(4)
+mtext("degrees",side=4,line=3)
+mtext("trips",side=2,line=3)
+legend("topright",col=c("black","blue"),lty=1,legend=c("trips","degrees"))
 
-plot(both)
-temps = read.csv('~/code/citibike/temperatures/temps.csv')
-lines(temps$datetime, temps$temp, type='l')
+
+# Correlate
+ccf(drop(departures), drop(both), na.action = na.pass)
+
+# Monte Carlo
+rand1 = sample(nrow(departures))
+rand2 = sample(nrow(both))
+ccf(drop(rand1), drop(rand2), na.action = na.pass)
+cor(rand1, rand2, use = "pairwise.complete.obs")
