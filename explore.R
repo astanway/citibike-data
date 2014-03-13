e562ba8885908483407ceaa5c6230e359a920550
Abe Stanway <abestanway@gmail.com>
2014-03-12 20:20:39 -0400

    Analysis

--- explore.R
+++ explore.R
@@ -1,63 +1,43 @@
-dat = read.csv("http://data.citibik.es/render/?from=-1weeks&target=9-Ave-%26-W-22-St.available_bikes&format=csv", header=TRUE)
-dat = setNames(dat, c("station", "date", "bike"))
-dat$date = as.POSIXct(dat$date)
-current = 1
-arrivals = c()
-departures = c()
-combined = c()
-for (i in 1:length(dat$date)) {
-  n = dat$bike[i]
-  if(is.na(n)){
-    next
-  }
-  if (n > current) {
-    arrivals = append(arrivals, dat$date[i])
-    combined = append(combined, dat$date[i])
-  }
-  if (n < current) {
-    departures = append(departures, dat$date[i])
-    combined = append(combined, dat$date[i])
-  }
-  current = n
-}
-
-# ! HOW TO BUCKET EVENT DATA !
-
-one = rep(c(1), length(arrivals))
-com = data.frame(arrivals, one)
-com = xts(com[,-1], order.by=com[,1])
-arrivals = period.apply(com, endpoints(com, "hours"), sum)
-
-# Get the trips
+# Get the departures
 json_file <- "~/code/citibike/departures.json"
 json_data <- fromJSON(paste(readLines(json_file), collapse=""))
 date = names(unlist(json_data))
-date = as.POSIXlt(as.numeric(date), origin="1970-01-01")
+date = round(as.POSIXlt(as.numeric(date), origin="1970-01-01"), "hours")
 values = unname(unlist(json_data))
 com = data.frame(date, values)
 com = xts(com[,-1], order.by=com[,1])
+
+# Aggregate by day
 departures = period.apply(com, endpoints(com, "days"), sum)
+
+# Plot
 plot(departures)
 
-# Plot the temps
-par(new = TRUE)
+# Get the temperatures
 temps = read.csv("~/code/citibike/temperatures/temps.csv", header=TRUE)
-temps$datetime = as.POSIXct(temps$datetime)
+temps$datetime = round(as.POSIXct(temps$datetime), "hours")
 com = data.frame(temps$datetime, temps$temp)
 com = xts(com[,-1], order.by=com[,1])
+
+#Aggregate by day
 both = period.apply(com, endpoints(com, "days"), mean)
+
+# Plot
+par(new = TRUE)
 plot(both, type='l', col="blue", lwd=2, ylim =c(-0, 100),xaxt="n",yaxt="n",xlab="",ylab="")
 axis(4)
-mtext("degrees",side=4,line=3)
+mtext("degrees fahrenheit",side=4,line=3)
 mtext("trips",side=2,line=3)
-legend("topright",col=c("black","blue"),lty=1,legend=c("trips","degrees"))
-
+legend("topright",col=c("black","blue"),lty=1,legend=c("departures","degrees"))
 
 # Correlate
+both = tail(both, length(departures))
+merged = merge(departures, both)
 ccf(drop(departures), drop(both), na.action = na.pass)
+cor(merged$departures, merged$both, use='complete.obs')
 
 # Monte Carlo
 rand1 = sample(nrow(departures))
 rand2 = sample(nrow(both))
 ccf(drop(rand1), drop(rand2), na.action = na.pass)
-cor(rand1, rand2, use = "pairwise.complete.obs")
+cor(rand1, rand2, use='complete.obs')
