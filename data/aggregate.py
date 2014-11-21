24488ed291d4541f21a9692cc56a90a7e1cf605e
Abe Stanway <abestanway@gmail.com>
2014-11-21 02:20:31 +0000

    Data dump for David Ma

--- data/aggregate.py
+++ data/aggregate.py
@@ -1,5 +1,5 @@
 # run this on the server
-
+from multiprocessing import Pool
 import urllib2
 import simplejson as json
 from collections import defaultdict
@@ -9,18 +9,19 @@ url = "http://data.citibik.es/metrics/index.json"
 page = urllib2.urlopen(url)
 content = page.read()
 stations = json.loads(content)
-for station in stations:
+
+def process_station(station):
     departures = defaultdict(int)
     arrivals = defaultdict(int)
     available = defaultdict(int)
     try:
         print station
         if "statsd" in station:
-            continue
+            return
         if "velocity" in station:
-            continue
+            return
         if "carbon" in station:
-            continue
+            return
         f = "/opt/graphite/storage/whisper/" + station.replace('.','/') + ".wsp"
         raw = subprocess.Popen(["whisper-fetch.py", f, "--from=0"], stdout=subprocess.PIPE).communicate()[0]
         current = None
@@ -47,21 +48,25 @@ for station in stations:
         print e
         print current
         station = station.replace('.available_bikes', '-').replace('&', 'and')
-        with open(station+'arrivals.csv', 'w') as outfile:
+        with open('arrivals/' + station + 'arrivals.csv', 'w') as outfile:
             outfile.write('datetime,arrivals\n')
             for key in sorted(arrivals.iterkeys()):
                 outfile.write(str(key) + ',' + str(arrivals[key]) + '\n')
             outfile.close()
 
-        with open(station+'departures.csv', 'w') as outfile:
+        with open('departures/' + station + 'departures.csv', 'w') as outfile:
             outfile.write('datetime,departures\n')
             for key in sorted(departures.iterkeys()):
                 outfile.write(str(key) + ',' + str(departures[key]) + '\n')
             outfile.close()
 
-        with open(station+'available.csv', 'w') as outfile:
+        with open('available/' + station + 'available.csv', 'w') as outfile:
             outfile.write('datetime,available_bikes\n')
             for key in sorted(available.iterkeys()):
                 outfile.write(str(key) + ',' + str(available[key]) + '\n')
             outfile.close()
-        continue 
+
+pool = Pool()
+results = pool.map(process_station, stations)
+pool.close()
+pool.join()
